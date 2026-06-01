fd346f61d4eb162cbd948ec10e9b7aa7e0d00433
laug <5089281+laug@users.noreply.github.com>
2026-06-01 12:46:46 +0900

    simplify eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import eslintPluginImportX from "eslint-plugin-import-x";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -6,19 +8,24 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { ignores: ["dist/**"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
-    files: ["**/*.{jsx,tsx}"],
-  },
-  {
-    ...pluginReact.configs.flat["jsx-runtime"],
-    files: ["**/*.{jsx,tsx}"],
-  },
-  {
-    files: ["**/*.{jsx,tsx}"],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: [
+      "js/recommended",
+      eslintPluginImportX.flatConfigs.recommended,
+      tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      pluginReact.configs.flat["jsx-runtime"],
+    ],
+    languageOptions: { globals: globals.browser },
     settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          project: "./tsconfig.app.json",
+          alwaysTryTypes: true,
+        }),
+      ],
       react: {
         version: "19.0",
       },
